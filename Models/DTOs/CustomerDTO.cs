b897c2f73ad0532a94246dc5783629ae6a7c50dc
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-04-09 19:11:49 -0500

    Implemented DTO classes

---
+++ Models/DTOs/CustomerDTO.cs
@@ -0,0 +1,8 @@
+namespace HoneyRaesApi.Models.DTOs;
+
+public class CustomerDTO()
+{
+    public int Id {get; set;}
+    public string Name {get; set;} = string.Empty;
+    public string Address {get; set;} = string.Empty;
+}
---
+++ Models/DTOs/EmployeeDTO.cs
@@ -0,0 +1,8 @@
+namespace HoneyRaesApi.Models.DTOs;
+
+public class EmployeeDTO()
+{
+    public int Id {get; set;}
+    public string Name {get; set;} = string.Empty;
+    public string Specialty {get; set;} = string.Empty;
+}
