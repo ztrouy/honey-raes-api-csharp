29e1e4d902375a00fa6955c3fd28ec08d49bb973
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-04-11 10:53:22 -0500

    Updated potentially null properties to be nullable

--- Models/DTOs/ServiceTicketDTO.cs
+++ Models/DTOs/ServiceTicketDTO.cs
@@ -4,9 +4,9 @@ public class ServiceTicketDTO
 {
     public int Id {get; set;}
     public int CustomerId {get; set;}
-    public int EmployeeId {get; set;}
+    public int? EmployeeId {get; set;}
     public string Description {get; set;} = string.Empty;
     public bool Emergency {get; set;}
-    public DateTime DateCompleted {get; set;}
+    public DateTime? DateCompleted {get; set;}
     public EmployeeDTO? Employee {get; set;}
 }
--- Models/ServiceTicket.cs
+++ Models/ServiceTicket.cs
@@ -4,9 +4,9 @@ public class ServiceTicket
 {
     public int Id {get; set;}
     public int CustomerId {get; set;}
-    public int EmployeeId {get; set;}
+    public int? EmployeeId {get; set;}
     public string Description {get; set;} = string.Empty;
     public bool Emergency {get; set;}
-    public DateTime DateCompleted {get; set;}
+    public DateTime? DateCompleted {get; set;}
     public Employee? Employee {get; set;}
 }
