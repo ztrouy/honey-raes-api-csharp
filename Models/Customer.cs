72367beee8c27fb913c26be7e331252914710332
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-04-11 11:11:14 -0500

    Added service ticket list property to store related service tickets

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -5,4 +5,5 @@ public class Customer
     public int Id {get; set;}
     public string Name {get; set;} = string.Empty;
     public string Address {get; set;} = string.Empty;
+    public List<ServiceTicket>? ServiceTickets {get; set;}
 }
--- Models/DTOs/CustomerDTO.cs
+++ Models/DTOs/CustomerDTO.cs
@@ -5,4 +5,5 @@ public class CustomerDTO
     public int Id {get; set;}
     public string Name {get; set;} = string.Empty;
     public string Address {get; set;} = string.Empty;
+    public List<ServiceTicketDTO>? ServiceTickets {get; set;}
 }
