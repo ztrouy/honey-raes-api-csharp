12b9e38e56d762dfb81c772a8e920a0e7a1e874b
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-04-11 10:34:26 -0500

    Added List property to hold service tickets attached to the employee

--- Models/DTOs/EmployeeDTO.cs
+++ Models/DTOs/EmployeeDTO.cs
@@ -5,4 +5,5 @@ public class EmployeeDTO
     public int Id {get; set;}
     public string Name {get; set;} = string.Empty;
     public string Specialty {get; set;} = string.Empty;
+    public List<ServiceTicketDTO> ServiceTickets {get; set;}
 }
--- Models/Employee.cs
+++ Models/Employee.cs
@@ -5,4 +5,5 @@ public class Employee
     public int Id {get; set;}
     public string Name {get; set;} = string.Empty;
     public string Specialty {get; set;} = string.Empty;
+    public List<ServiceTicket> ServiceTickets {get; set;}
 }
